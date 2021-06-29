a481d63b82b796deef81404ba685d8c7b8fb9fac
Romy Lynch <romylynch@gmail.com>
2021-06-29 11:29:03 +0100

    change

--- api/index.rb
+++ api/index.rb
@@ -1,5 +1,5 @@
 require 'json'
-file = File.read(../lib/zapier/zapfile.json)
+file = File.read("../lib/zapier/zapfile.json")
 data_hash = JSON.parse(file)
 response = data_hash["zaps"].map do |zap|
   names = zap["nodes"].to_a.map { |node| node[1]["selected_api"] }
