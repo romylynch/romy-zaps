bf82f08ef377943849162715df4e79b1de0c055f
Romy Lynch <romylynch@gmail.com>
2021-06-29 14:25:38 +0100

    update 2

--- api/index.rb
+++ api/index.rb
@@ -1,3 +1,12 @@
+require 'json'
+
+file = File.read("../lib/zapier/zap_data.json")
+data_hash = JSON.parse(file)
+response = data_hash["zaps"].map do |zap|
+  names = zap["nodes"].to_a.map { |node| node[1]["selected_api"] }
+  {title: zap["title"], names: names}
+end
+
 Handler = Proc.new do |req, res|
   res.status = 200
   res['Content-Type'] = 'text/text; charset=utf-8'
